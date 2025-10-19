0ecd873141fd55b1c7b9eddeeaa92315ee72b741
Anan-Saleh-1995 <anansaleh1995@gmail.com>
2025-10-19 18:35:47 +0300

    Import and use Password generator in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-export const App = () => {
+import { PasswordGenerator } from '@/components/password';
 
+export const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="flex justify-center min-h-screen p-4 lg:items-center">
+      <PasswordGenerator />
+    </div>
+  );
+};
